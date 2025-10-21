c2a3434b2843f2284b5d4491923fcb5193cc8736
YujieSu <2280914133s@gmail.com>
2025-10-21 15:59:03 -0700

    Regular updates

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/autonomous/Red.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/autonomous/Red.java
@@ -1,15 +1,46 @@
 package org.firstinspires.ftc.teamcode.autonomous;
 
+import androidx.annotation.NonNull;
+
 import com.acmerobotics.dashboard.config.Config;
+import com.acmerobotics.dashboard.telemetry.TelemetryPacket;
+import com.acmerobotics.roadrunner.Action;
+import com.acmerobotics.roadrunner.Pose2d;
+import com.acmerobotics.roadrunner.SequentialAction;
+import com.acmerobotics.roadrunner.Vector2d;
+import com.acmerobotics.roadrunner.ftc.Actions;
+import com.acmerobotics.roadrunner.TrajectoryActionBuilder;
+
 
 import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.hardware.HardwareMap;
+import com.qualcomm.robotcore.hardware.Servo;
+import com.qualcomm.robotcore.hardware.DcMotorEx;
+import org.firstinspires.ftc.teamcode.MecanumDrive;
+import org.firstinspires.ftc.teamcode.teleOp.GoBildaPinpointDriver;
+
+import org.firstinspires.ftc.teamcode.subsystems.Elevator;
+import org.firstinspires.ftc.teamcode.subsystems.Rotator;
+import org.firstinspires.ftc.teamcode.subsystems.Shooter;
 
 
-public class Red {
-    @Config
-    @Autonomous(name = "AUTONOMOUS_RED", group = "Autonomous")
-    public class Red extends LinearOpMode {
-        
+@Config
+@Autonomous(name = "AUTONOMOUS_RED", group = "Auto")
+public class Red extends LinearOpMode {
+    @Override
+    public void runOpMode() throws InterruptedException {
+        Pose2d initialPose = new Pose2d(0, 0, Math.toRadians(-90));
+
+
+
+        TrajectoryActionBuilder traj1, traj2;
+
+        //traj1 = .actionBuilder(initialPose).strafeTo(new Pose2d(0, 0, Math.toRadians(0)));
+        Actions.runBlocking(
+                new SequentialAction(
+
+                )
+        );
     }
 }
--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/subsystems/Drivetrain.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/subsystems/Drivetrain.java
@@ -1,4 +1,4 @@
-package org.firstinspires.ftc.teamcode;
+package org.firstinspires.ftc.teamcode.subsystems;
 
 import androidx.annotation.NonNull;
 
@@ -17,12 +17,36 @@ import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.hardware.HardwareMap;
 import com.qualcomm.robotcore.hardware.Servo;
 import com.qualcomm.robotcore.hardware.DcMotorEx;
-import org.firstinspires.ftc.teamcode.MecanumDrive_rr;
+
+import org.firstinspires.ftc.robotcore.external.navigation.DistanceUnit;
+import org.firstinspires.ftc.teamcode.MecanumDrive;
+import org.firstinspires.ftc.teamcode.teleOp.GoBildaPinpointDriver;
 
 public class Drivetrain {
-    private DcMotorEx leftFront, leftBack, rightFront, rightBack;
+    private DcMotorEx leftFrontMotor, leftBackMotor, rightFrontMotor, rightBackMotor;
+    static public GoBildaPinpointDriver odo;
 
     public Drivetrain(HardwareMap hwMap) {
+        leftFrontMotor  = hwMap.get(DcMotorEx.class, "leftFrontMotor");
+        leftBackMotor  = hwMap.get(DcMotorEx.class, "leftBackMotor");
+        rightFrontMotor = hwMap.get(DcMotorEx.class, "rightFrontMotor");
+        rightBackMotor = hwMap.get(DcMotorEx.class, "rightBackMotor");
 
+        leftFrontMotor.setDirection(DcMotorEx.Direction.REVERSE);
+        leftBackMotor.setDirection(DcMotorEx.Direction.REVERSE);
+        rightFrontMotor.setDirection(DcMotorEx.Direction.FORWARD);
+        rightBackMotor.setDirection(DcMotorEx.Direction.FORWARD);
+
+        odo = hwMap.get(GoBildaPinpointDriver.class, "odo");
+        odo.setOffsets(-0.4, 3.4, DistanceUnit.INCH);
+
+
+    }
+
+    public void drive(double leftFrontSpeed, double leftBackSpeed, double rightFrontSpeed, double rightBackSpeed) {
+        leftFrontMotor.setPower(leftFrontSpeed);
+        leftBackMotor.setPower(leftBackSpeed);
+        rightFrontMotor.setPower(rightFrontSpeed);
+        rightBackMotor.setPower(rightBackSpeed);
     }
 }
