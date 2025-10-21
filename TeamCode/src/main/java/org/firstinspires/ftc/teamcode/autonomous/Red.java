10f4c205f6e3d07909a6811569c386f32b1a4df3
YujieSu <2280914133s@gmail.com>
2025-10-21 15:58:50 -0700

    Regular updates

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/autonomous/Red.java
@@ -0,0 +1,15 @@
+package org.firstinspires.ftc.teamcode.autonomous;
+
+import com.acmerobotics.dashboard.config.Config;
+
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+
+
+public class Red {
+    @Config
+    @Autonomous(name = "AUTONOMOUS_RED", group = "Autonomous")
+    public class Red extends LinearOpMode {
+        
+    }
+}
